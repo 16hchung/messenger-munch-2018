c286c4cf6aa62bdfb23a1e307c35ad5d7d281995
Heejung Chung <16hchung@gmail.com>
2018-01-02 21:09:35 -0800

    continuing to debug setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // Imports dependencies and set up http server
 const
+	token = process.env.FB_PAGE_ACCESS_TOKEN
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
